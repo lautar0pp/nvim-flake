d54f6367124e0245d6b377763ce63403f82756fe
lautar0pp <lautaro_RL@outlook.com>
2024-03-01 14:37:26 -0300

    I dont kno

--- nvim/plugin/harpoon.lua
+++ nvim/plugin/harpoon.lua
@@ -6,9 +6,8 @@ vim.g.did_load_harpoon_plugin = true
 local harpoon = require("harpoon")
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
