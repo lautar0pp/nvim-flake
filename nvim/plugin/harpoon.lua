f9748731d0a4ce99c476d8cc935dcef7bce0918a
lautar0pp <lautaro_RL@outlook.com>
2024-03-10 15:52:48 -0300

    harpoon config test

--- nvim/plugin/harpoon.lua
+++ nvim/plugin/harpoon.lua
@@ -1,15 +1,14 @@
-local harpoon = require("harpoon")
+-- local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+require("harpoon"):setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() require("harpoon"):list():append() end)
 
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end)
+vim.keymap.set("n", "<C-h>", function() require("harpoon"):list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() require("harpoon"):list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() require("harpoon"):list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() require("harpoon"):list():select(4) end)
 
-print("help")
