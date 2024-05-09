15031699d3be770ffb7a1308c120b850a03ddc5f
lautar0pp <lautaro_RL@outlook.com>
2024-05-09 11:46:14 -0300

    removed sqlite in init

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,9 +93,6 @@ vim.diagnostic.config {
 vim.cmd.filetype('plugin', 'indent', 'on')
 vim.cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
--- let sqlite.lua (which some plugins depend on) know where to find sqlite
-vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
-
 -- this should be at the end, because
 -- it causes neovim to source ftplugins
 -- on the packpath when passing a file to the nvim command
